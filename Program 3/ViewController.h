1386580fee21b32d4c95ba5640b81a446f0acf1b
swift <sixfold.54-county@icloud.com>
2025-06-15 21:17:42 +0100

    Comparison X and Y , using Objective-C and Storyboard for GUI  .

--- Program 3/ViewController.h
+++ Program 3/ViewController.h
@@ -9,6 +9,12 @@
 
 @interface ViewController : NSViewController
 
+@property (weak) IBOutlet NSTextField *textFieldX;
+@property (weak) IBOutlet NSTextField *textFieldY;
+@property (weak) IBOutlet NSTextField *resultLabel;
+// This outlet should be connected in the storyboard to the label or text field where you want the result to appear.
+
+- (IBAction)compareButtonClicked:(id)sender;
 
 @end
 
--- Program 3/ViewController.m
+++ Program 3/ViewController.m
@@ -1,26 +1,30 @@
-//
-//  ViewController.m
-//  Program 3
-//
-//  Created by swift on 15/06/2025.
-//
-
 #import "ViewController.h"
 
+@interface ViewController ()
+
+@end
+
 @implementation ViewController
+@synthesize textFieldX;
+@synthesize textFieldY;
+@synthesize resultLabel;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
     // Do any additional setup after loading the view.
 }
 
+- (IBAction)compareButtonClicked:(id)sender {
+    NSInteger x = [self.textFieldX.stringValue integerValue];
+    NSInteger y = [self.textFieldY.stringValue integerValue];
 
-- (void)setRepresentedObject:(id)representedObject {
-    [super setRepresentedObject:representedObject];
-
-    // Update the view, if already loaded.
+    if (x > y) {
+        self.resultLabel.stringValue = [NSString stringWithFormat:@"X (%ld) is greater than Y (%ld)", (long)x, (long)y];
+    } else if (x < y) {
+        self.resultLabel.stringValue = [NSString stringWithFormat:@"X (%ld) is less than Y (%ld)", (long)x, (long)y];
+    } else {
+        self.resultLabel.stringValue = [NSString stringWithFormat:@"X (%ld) is equal to Y (%ld)", (long)x, (long)y];
+    }
 }
-
 
 @end
