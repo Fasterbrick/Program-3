5c11cdef7b2ef8d344127745359a8de38d3fad2c
swift <sixfold.54-county@icloud.com>
2025-06-15 20:28:14 +0100

    Initial Commit

---
+++ Program 3/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Program 3
+//
+//  Created by swift on 15/06/2025.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ Program 3/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  Program 3
+//
+//  Created by swift on 15/06/2025.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+
+@end
